d3233550451b5a76e7a978edfa7f0399f27adc32
brunavabs <bru.almeida12@gmail.com>
2022-04-24 15:42:55 -0300

    first commit

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const app = express();
 app.use (cors());
 app.use(routes);
 
-app.listen(4000, () => console.log("Servidor rodando an porta 4000"));
+app.listen(4000, () => console.log("Servidor rodando na porta 4000"));
